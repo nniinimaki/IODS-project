129444f4fb780715ea4d4781b60db1f1d9c7a8a0
Niinimäki <nniinima@ad.helsinki.fi>
2022-11-21 16:13:18 +0200

    Assignment 3 data wrangling

---
+++ data/create_alc.R
@@ -0,0 +1,7 @@
+# Niina Niinimäki
+# 21.11.2022
+# IODS, Assignment 3
+# Data source: https://archive.ics.uci.edu/ml/datasets/Student+Performance
+
+# Read student-mat.csv and student-por.csv into R (from the data folder) and explore the structure and dimensions of the data
+math <- read.table(url_math, sep = ";" , header = TRUE)
